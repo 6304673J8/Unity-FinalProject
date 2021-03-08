d077e23ee8926c0fec6db6edc0887a3a3c500c34
J0 <petergeorghe.julca@enti.cat>
2021-03-08 20:50:28 +0100

    Susana Movement
    New Input System Setup

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,176 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/PlayerInput.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerInput : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerInput()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerInput"",
+    ""maps"": [
+        {
+            ""name"": ""Floor"",
+            ""id"": ""0989f67f-8947-4487-bdbc-969ec1149cea"",
+            ""actions"": [
+                {
+                    ""name"": ""Walking"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""54b0a47a-78c6-481e-a1ce-212fdd7b016b"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": ""NormalizeVector2"",
+                    ""interactions"": ""Press""
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""8dcf315c-5307-4594-ad17-f5e41442512c"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walking"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""d9745a7e-b2ff-4066-bf4b-448ab04c1c4a"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walking"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""402e7898-42be-4ce8-831a-4aa26d9539f0"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walking"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""e10a3016-fecf-421d-8208-0c810ec65cb0"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walking"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""3b16ea5e-cadd-4408-9aaf-64fd9ac3c59a"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walking"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Floor
+        m_Floor = asset.FindActionMap("Floor", throwIfNotFound: true);
+        m_Floor_Walking = m_Floor.FindAction("Walking", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Floor
+    private readonly InputActionMap m_Floor;
+    private IFloorActions m_FloorActionsCallbackInterface;
+    private readonly InputAction m_Floor_Walking;
+    public struct FloorActions
+    {
+        private @PlayerInput m_Wrapper;
+        public FloorActions(@PlayerInput wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Walking => m_Wrapper.m_Floor_Walking;
+        public InputActionMap Get() { return m_Wrapper.m_Floor; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(FloorActions set) { return set.Get(); }
+        public void SetCallbacks(IFloorActions instance)
+        {
+            if (m_Wrapper.m_FloorActionsCallbackInterface != null)
+            {
+                @Walking.started -= m_Wrapper.m_FloorActionsCallbackInterface.OnWalking;
+                @Walking.performed -= m_Wrapper.m_FloorActionsCallbackInterface.OnWalking;
+                @Walking.canceled -= m_Wrapper.m_FloorActionsCallbackInterface.OnWalking;
+            }
+            m_Wrapper.m_FloorActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Walking.started += instance.OnWalking;
+                @Walking.performed += instance.OnWalking;
+                @Walking.canceled += instance.OnWalking;
+            }
+        }
+    }
+    public FloorActions @Floor => new FloorActions(this);
+    public interface IFloorActions
+    {
+        void OnWalking(InputAction.CallbackContext context);
+    }
+}
--- Assets/Scripts/SusanaController.cs
+++ Assets/Scripts/SusanaController.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class PlayerMovement : MonoBehaviour
+public class SusanaController : MonoBehaviour
 {
+    [SerializeField]
+    private Tilemap floorTilemap;
+    [SerializeField]
+    private Tilemap collisionTilemap;
+
+    private PlayerInput controller;
     //Movimiento Susana
     public float mSpeed = 5.0f; //Velocidad de movimiento de Susana
     public Transform movePivot;
@@ -15,15 +22,48 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private BoxCollider2D boxCollider;
 
+    private void Awake()
+    {
+        controller = new PlayerInput();    
+    }
+
+    private void OnEnable()
+    {
+        controller.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controller.Disable();
+    }
     // Start is called before the first frame update
     void Start()
     {
+        controller.Floor.Walking.performed += ctx => Move(ctx.ReadValue<Vector2>());
         movePivot.parent = null;
         rb = GetComponent<Rigidbody2D>();
     }
 
+    private void Move(Vector2 dir)
+    {
+        if (CanMove(dir))
+        {
+            transform.position += (Vector3)dir;
+        }
+    }
+
+    private bool CanMove(Vector2 dir)
+    {
+        Vector3Int gridPos = floorTilemap.WorldToCell(transform.position + (Vector3)dir);
+        if (!floorTilemap.HasTile(gridPos) || collisionTilemap.HasTile(gridPos))
+        {
+            return false;
+        }
+        return true;
+    }
+
     // Update is called once per frame
-    void Update()
+    /*void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
@@ -31,20 +71,19 @@ public class PlayerMovement : MonoBehaviour
         if (Vector3.Distance(transform.position, movePivot.position) <= .05f) {
             if (Mathf.Abs(movement.x) == 1f)
             {
-                movePivot.position += new Vector3(movement.x, 0f, 0f);
+                movePivot.position += new Vector3(movement.x * 3, 0f, 0f);
             } else if (Mathf.Abs(movement.y) == 1f)
             {
-                movePivot.position += new Vector3(0, movement.y, 0f);
+                movePivot.position += new Vector3(0, movement.y * 3, 0f);
             }
         }
 
     }
-
     void FixedUpdate()
     {
         float delta = Time.fixedDeltaTime * 1000; //Milisegundos desde el último frame
 
         rb.MovePosition(rb.position + movement * mSpeed * Time.fixedDeltaTime);
 
-    }
+    }*/
 }
