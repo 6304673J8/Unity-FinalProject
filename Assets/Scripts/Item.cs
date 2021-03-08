9f2b27b4b743e040b2668e04c966a610de98a6dd
Gerardenti <gerard.benito@enti.cat>
2021-03-08 17:58:24 +0100

    Prototype Tiles, Floor and Walls

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+
+    private KeyCode pickupButton = KeyCode.Alpha1; //Tecla para que Susana recoja el objeto
+
+    private Rigidbody2D rigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
