12c80563593214d5612a47ea2a4f632a9971ae58
Albert <albert.faro@enti.cat>
2021-03-09 16:45:31 +0100

    Combat Manager created

---
+++ Assets/Scripts/CombatManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatManager : MonoBehaviour
+{
+
+    public static CombatManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+
+        else
+        {
+            Debug.Log("Warning, multiple" + this + "In this scene!");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+   void EnemyAttack()
+    {
+
+
+    }
+
+    void PlayerAttack()
+    {
+
+    }
+
+}
