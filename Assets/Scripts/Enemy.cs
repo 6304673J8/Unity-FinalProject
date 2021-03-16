2fe4c7388986f9fce64544fb6ffc9c82d3112154
Albert <albert.faro@enti.cat>
2021-03-16 22:21:30 +0100

    Enemy Tile Movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,56 +1,89 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Tilemaps;
 
 public class Enemy : MonoBehaviour
 {
     public Transform player;
-    public float moveSpeed = 1f;
-    public float range;
-    public int hp;
+
     public int damage;
-    private float lastAttackTime;
-    public float attackDelay = 1;
-    private Rigidbody2D enemyrb;
-    private Vector2 movement;
-    // Start is called before the first frame update 
-    void Start()
+
+    Rigidbody2D rb;
+
+    [SerializeField] float speed;
+
+    Vector2[] directions = { Vector2.up, Vector2.right, Vector2.down, Vector2.left };
+
+    int directionIndex = 0;
+
+    //Ver la direccion actual
+    [SerializeField] Vector2 currentDir;
+
+    //La distancia a la que va a mirar el raycast
+    [SerializeField] float rayDistance;
+
+    [SerializeField] LayerMask rayLayer;
+
+    private void Awake()
     {
-        enemyrb = this.GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Start()
+    {
+        currentDir = directions[directionIndex];
     }
 
-    // Update is called once per frame 
-    void Update()
-    {
-        Vector3 direction = player.position - transform.position;
-        direction.Normalize();
-        movement = direction;
 
-        //Comprobar la distancia entre el enemigo y el jugador
-        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
-        if(distanceToPlayer < range)
+    private void Update()
+    {
+        //Raycast
+        RaycastHit2D hit2D = Physics2D.Raycast(transform.position, currentDir,rayDistance, rayLayer);
+        Vector3 endpoint = currentDir * rayDistance;
+        Debug.DrawLine(transform.position, transform.position + endpoint, Color.green);
+
+        if(hit2D.collider != null)
         {
-            if(Time.time > lastAttackTime + attackDelay)
+            if(hit2D.collider.gameObject.CompareTag("Wall"))
             {
-                player.SendMessage("TakeDamage", damage);
-                      lastAttackTime = Time.time;
+                ChangeDirection();
             }
-           
-          
-          
+
+            if (hit2D.collider.gameObject.CompareTag("Breakable"))
+            {
+                ChangeDirection();
+            }
+
+            if (hit2D.collider.gameObject.CompareTag("Susana"))
+            {
+                //Hace falta poner a Susana en otra Layer que no sea Default!
+                player.SendMessage("TakeDamage", damage);
+            }
         }
     }
 
+
+    void ChangeDirection()
+    {
+        //Decide una dirección de forma aleatoria (En el array de direcciones)
+        directionIndex += Random.Range(0, 2) * 2 - 1;
+
+        //Hace que el índice no supere 3
+        int clampedIndex = directionIndex % directions.Length;
+
+        //Hace que el índice sea positivo
+        if (clampedIndex < 0)
+        {
+            clampedIndex = directions.Length + clampedIndex;
+        }
+        rb.velocity = Vector2.zero;
+
+        currentDir = directions[clampedIndex];
+    }
     private void FixedUpdate()
     {
-        moveEnemy(movement);
+        rb.AddForce(currentDir * speed); 
     }
 
-    void moveEnemy(Vector2 direction)
-    {
-        enemyrb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
-    }
 
-  
 }
