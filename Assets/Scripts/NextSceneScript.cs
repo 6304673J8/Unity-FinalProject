969a01d1d80cc6042b2582af552794c9412814de
Albert <albert.faro@enti.cat>
2021-04-05 20:20:08 +0200

    Intro
    Game intro before the tutorial

---
+++ Assets/Scripts/NextSceneScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NextSceneScript : MonoBehaviour
+{
+
+    private float timer = 45f;
+
+    private float elapsed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        elapsed += Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsed += Time.deltaTime;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene("TutorialRoom", LoadSceneMode.Single);
+        }
+
+        if(elapsed>=timer)
+        {
+            SceneManager.LoadScene("TutorialRoom", LoadSceneMode.Single);
+        }
+    }
+}
