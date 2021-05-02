6cb8123e1a174d7a17b20c29f6640594357e4806
J0 <petergeorghe.julca@enti.cat>
2021-05-03 00:44:32 +0200

    Statue Behaviour
    ToDo Button UI

---
+++ Assets/Scripts/Controlled/SaveSystem/SaveSystem.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem
+{
+    public static void SaveSusana(SusanaControlled susana)
+    {
+        BinaryFormatter fomatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/susana.ytano";
+
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        SusanaData data = new SusanaData(susana);
+        
+        //Writing data to the file
+        fomatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static SusanaData LoadSusana ()
+    {
+        string path = Application.persistentDataPath + "/susana.ytano";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            SusanaData data = formatter.Deserialize(stream) as SusanaData;
+            stream.Close();
+            return data;
+        }
+        else
+        {
+            Debug.LogError("Susana Data not found in " + path);
+            return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Controlled/SaveSystem/SusanaData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SusanaData {
+    //PLAYER STATUS
+    //int level;
+    public int health;
+    public float[] position;
+    
+    //PLAYER INVENTORY
+    public int potions;
+    public int keys;
+    public int bombs;
+
+    public SusanaData (SusanaControlled susana)
+    {
+        //level = susana.level;
+        health = susana.health;
+        position = new float[3];
+        position[0] = susana.transform.position.x;
+        position[1] = susana.transform.position.y;
+        position[2] = susana.transform.position.z;
+        
+        potions = susana.nPotions;
+        keys = susana.nKeys;
+        bombs = susana.nBombs;
+    }
+}
