c5be7584bcf83d77138b53b2144e08cee8471045
Albert <albert.faro@enti.cat>
2021-05-04 20:09:16 +0200

    Sound Test Fireball

--- Assets/Scripts/FireBall.cs
+++ Assets/Scripts/FireBall.cs
@@ -19,6 +19,8 @@ public class FireBall : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
+
         player = GameObject.FindGameObjectWithTag("Susana").transform;
 
         playerR = GameObject.FindGameObjectWithTag("Susana");
