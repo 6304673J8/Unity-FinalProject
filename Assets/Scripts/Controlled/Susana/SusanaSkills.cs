e5dc7bf2570534fccab88b171e62e07ea0b01240
Georghe Julca <petergeorghe.julca@enti.cat>
2021-05-07 15:22:40 +0200

    Release - Workplace CleanUp
    Order in Chaos

---
+++ Assets/Scripts/Controlled/Susana/SusanaSkills.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SusanaSkills : MonoBehaviour
+{
+    [Header("Player")]
+    public Rigidbody2D rb;
+
+    [Header("Abilities")]
+
+    public GameObject earthquakePrefab;
+    public GameObject lungePrefab;
+    public GameObject potionPrefab;
+
+    public void Awake()
+    {
+    }
+    public void EarthquakeLogic()
+    {
+        Vector2 pos = rb.transform.position;
+
+        GameObject earthquakeFX = Instantiate(earthquakePrefab, pos, rb.transform.rotation);
+    }
+
+    public void LungeLogic()
+    {
+        Vector2 pos = rb.transform.position;
+
+        GameObject lungeFX = Instantiate(lungePrefab, pos, rb.transform.rotation);
+    }
+
+    public void PotionLogic()
+    {
+        Vector2 pos = rb.transform.position;
+
+        GameObject potionFX = Instantiate(potionPrefab, pos, rb.transform.rotation);
+    }
+}
---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,250 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Enemy : MonoBehaviour
+{
+    public float timerSpeed; //Este valor define los segundos del temporizador
+
+    private float stunTimer = 4f; //Temporizador del STUN
+
+    private float timerSpeedAttack = 0.8f; //Temporizador del ataque (ataca a los 0.8 segundos de estar en rango)
+
+    public float attackDelay = 1;
+
+    private float lastAttackTime;
+
+    private float elapsed;
+
+    private float elapsedF;
+
+    private float elapsedStun;
+
+    private Animator animator;
+
+    SpriteRenderer sprite;
+
+    public int hp;
+
+    private bool stunned;
+
+    public Transform player;
+
+    public int damage;
+
+    public int range;
+
+    Rigidbody2D rb;
+
+
+    [SerializeField]
+    private Tilemap floorTilemap;
+    [SerializeField]
+    private Tilemap collisionTilemap;
+    [SerializeField]
+    private Tilemap floorItemsTilemap;
+    [SerializeField]
+    private Tilemap breakablesTilemap;
+
+    /*[SerializeField]
+    private Tilemap damagingTilemap;*/
+
+
+
+    Vector2[] directions = { Vector2.up, Vector2.right, Vector2.down, Vector2.left };
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+
+    }
+    private void Start()
+    {
+        elapsed += Time.deltaTime;
+        stunned = false;
+        
+    }
+
+
+    private void Update()
+    {
+       if(hp <= 0)
+        {
+            kill();
+        }
+
+    }
+
+
+    
+    private void FixedUpdate()
+    {
+        if(!stunned)
+        {
+            float separation = Vector3.Distance(this.transform.position, player.transform.position);
+            elapsed += Time.deltaTime;
+            if (elapsed >= timerSpeed)
+            {
+                elapsed = 0f;
+
+
+                Debug.Log("Range to player = " + separation);
+
+                if (separation <= range)
+                {
+                    Debug.Log("The enemy sees the player");
+                    followPlayer();
+                }
+
+                else if (separation > range)
+                {
+                    
+                    Debug.Log("The enemy is patrolling");
+                    Patrol();
+                }
+            }
+
+
+            if (separation <= 1)
+            {
+                elapsedF += Time.deltaTime;
+                if (elapsedF >= timerSpeedAttack)
+                {
+                    elapsedF = 0;
+                    attackPlayer();
+                }
+
+
+            }
+
+            if(separation > 1)
+            {
+                animator.ResetTrigger("isAttacking");
+                animator.SetTrigger("isWalking");
+            }
+        }
+
+
+        else
+        {
+          elapsedStun += Time.deltaTime;
+        if (elapsedStun >= stunTimer)
+        {
+           elapsedStun = 0f;
+           unStun();
+        }
+
+
+        }
+
+    }
+
+    void attackPlayer()
+    {
+        if (Time.time > lastAttackTime + attackDelay)
+        {
+            animator.SetTrigger("isAttacking");
+            Debug.Log("Player Taking Damage");
+            player.SendMessage("TakeDamage", damage);
+            lastAttackTime = Time.time;
+            
+        }
+    }
+
+    void Patrol()
+    {
+        int dir = Random.Range(0, 3);
+        Move(directions[dir]);
+    }
+
+
+    private void Move(Vector2 dir)
+    {
+        if (CanMove(dir))
+        {
+            dir = new Vector2Int(Mathf.FloorToInt(dir.x), Mathf.FloorToInt(dir.y));
+            transform.position += (Vector3)dir;
+            sprite.color = new Color(1, 1, 1, 1);
+
+        }
+
+
+    }
+
+    private bool CanMove(Vector2 dir)
+    {
+        Vector3Int gridPos = floorTilemap.WorldToCell(transform.position + (Vector3)dir);
+        if (!floorTilemap.HasTile(gridPos) || collisionTilemap.HasTile(gridPos) || floorItemsTilemap.HasTile(gridPos) || breakablesTilemap.HasTile(gridPos))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private void followPlayer()
+    {
+        if(player.position.x > rb.position.x + 1)
+        {
+            Move(directions[1]);
+            return;
+        }
+        else if(player.position.x < rb.position.x - 1)
+        {
+            Move(directions[3]);
+            return;
+        }
+        else if(player.position.y > rb.position.y + 1)
+        {
+            Move(directions[0]);
+            return;
+        }
+
+        else if (player.position.y < rb.position.y - 1)
+        {
+            Move(directions[2]);
+            return;
+        }
+
+
+    }
+
+    private void getStunned()
+    {
+        stunned = true;
+        sprite.color = new Color(0, 0, 1, 1);
+        hp -= 20;
+        return;
+        
+    }
+
+    private void unStun()
+    {
+        stunned = false;
+        sprite.color = new Color(1, 1, 1, 1);
+        return;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Earthquake"))
+        {
+           
+            getStunned();
+        }
+
+    }
+
+    private void kill()
+    {
+
+        Destroy(gameObject);
+    }
+
+    
+}
+
