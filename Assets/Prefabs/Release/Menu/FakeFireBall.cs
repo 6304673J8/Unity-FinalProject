9abc7f383bc34c8e8b40c303877503e922e0aff1
J0 <petergeorghe.julca@enti.cat>
2021-05-26 14:20:16 +0200

    Door Testing Sprite

---
+++ Assets/Prefabs/Release/Menu/FakeFireBall.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeFireBall : MonoBehaviour
+{
+    public GameObject playerR;
+    public int damage;
+    public float speed = 5;
+
+    private Transform player;
+    private Vector2 target;
+
+    private Animator animator;
+
+    private float beingDestroyed;
+
+    private float destroyTime = 0.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        player = GameObject.FindGameObjectWithTag("Susana").transform;
+
+        playerR = GameObject.FindGameObjectWithTag("Susana");
+
+        target = new Vector2(player.position.x, player.position.y);
+
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if (transform.position.x == target.x && transform.position.y == target.y)
+        {
+            DestroyProjectile();
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.tag == "Susana")
+        {
+            DestroyProjectile();
+        }
+
+        if (collision.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+
+        if (collision.tag == "Enemy")
+        {
+            DestroyProjectile();
+        }
+    }
+    void DestroyProjectile()
+    {
+
+        animator.SetTrigger("Destroyed");
+        beingDestroyed += Time.deltaTime;
+        if (beingDestroyed >= destroyTime)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assets/Prefabs/Release/Menu/FakeIA.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeIA : MonoBehaviour
+{
+    public Transform player;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    public float moveSpeed = 1f;
+    public float timerSpeed; //Este valor define los segundos del temporizador
+
+    private float stunTimer = 4f; //Temporizador del STUN
+
+    private float timerSpeedAttack = 0.3f; //Temporizador del ataque (ataca a los 0.8 segundos de estar en rango)
+
+    public float attackDelay = 0.3f;
+
+    private float lastAttackTime;
+
+    private float elapsed;
+
+    private float elapsedF;
+
+    private float elapsedStun;
+
+    private Animator animator;
+
+    SpriteRenderer sprite;
+
+    public int hp;
+
+    public int range;
+
+    public int originalRange;
+
+    private bool facingLeft;
+
+    public int enemyTag;
+
+    private void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+
+    }
+
+    private void Start()
+    {
+        facingLeft = true;
+        elapsed += Time.deltaTime;
+        rb = this.GetComponent<Rigidbody2D>();
+        originalRange = range;
+    }
+
+    private void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        
+        direction.Normalize();
+        movement = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        checkPosition();
+        float separation = Vector3.Distance(this.transform.position, player.transform.position);
+        elapsed += Time.deltaTime;
+
+        if (elapsed >= timerSpeed)
+        {
+            elapsed = 0f;
+
+
+            //Debug.Log("Range to player = " + separation);
+
+            if (separation <= range)
+            {
+                animator.ResetTrigger("isIdle");
+                animator.SetTrigger("isWalking");
+                chasePlayer(movement);
+                range = originalRange * 2;
+            }
+
+            else if (separation > range)
+            {
+                animator.ResetTrigger("isWalking");
+                animator.SetTrigger("isIdle");
+                range = originalRange;
+            }
+        }
+
+
+        if (separation <= 1)
+        {
+            elapsedF += Time.deltaTime;
+            if (elapsedF >= timerSpeedAttack)
+            {
+                elapsedF = 0;
+                attackPlayer();
+            }
+
+
+
+
+        }
+
+        if (separation <= 0.7)
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+
+        if (separation > 0.7)
+        {
+            rb.constraints = RigidbodyConstraints2D.None;
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+
+        if (separation > 1)
+        {
+            animator.ResetTrigger("isAttacking");
+            animator.SetTrigger("isWalking");
+
+        }
+    }
+
+    void chasePlayer(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+
+    }
+
+    private void kill()
+    {
+        if (enemyTag == 1)
+        {
+            FindObjectOfType<SoundManager>().Play("OnaosDeath");
+        }
+
+        else if (enemyTag == 2)
+        {
+            FindObjectOfType<SoundManager>().Play("PiamondDeath");
+        }
+        sprite.color = new Color(1, 1, 1, 1);
+        animator.ResetTrigger("isWalking");
+        animator.SetTrigger("isDying");
+    }
+
+    void attackPlayer()
+    {
+        animator.ResetTrigger("isAttacking");
+        animator.SetTrigger("isWalking");
+        if (Time.time > lastAttackTime + attackDelay)
+        {
+            animator.ResetTrigger("isWalking");
+            animator.SetTrigger("isAttacking");
+            Debug.Log("Player Taking Damage");
+            lastAttackTime = Time.time;
+
+        }
+    }
+
+    void Flip()
+    {
+        facingLeft = !facingLeft;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
+    void checkPosition()
+    {
+        if (facingLeft && player.position.x > transform.position.x)
+        {
+            Flip();
+        }
+
+        if (!facingLeft && player.position.x < transform.position.x)
+        {
+            Flip();
+        }
+    }
+}
