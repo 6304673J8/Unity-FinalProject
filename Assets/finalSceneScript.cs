1178f4cf3f8b729b4101983ca1b9a1f3b303039e
Albert <albert.faro@enti.cat>
2021-06-03 23:28:03 +0200

    Release - MainMenu&More

---
+++ Assets/finalSceneScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class finalSceneScript : MonoBehaviour
+{
+
+    private float timer = 19f;
+
+    private float elapsed;
+
+    private PlayerInputs sceneControls;
+
+
+    private void Awake()
+    {
+        sceneControls = new PlayerInputs();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        elapsed += Time.deltaTime;
+        sceneControls.Floor.Next.performed += ctx => MainMenuScene();
+    }
+
+
+    private void OnEnable()
+    {
+        sceneControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        sceneControls.Disable();
+    }
+
+    private void MainMenuScene()
+    {
+        SceneManager.LoadScene("MainMenu",LoadSceneMode.Single);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
