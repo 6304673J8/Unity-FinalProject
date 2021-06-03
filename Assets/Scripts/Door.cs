22be61e3b78129dadff4a95b3a2a6a05e875d1b2
J0 <petergeorghe.julca@enti.cat>
2021-06-04 00:24:14 +0200

    Door Release
    64px

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
 
-    //public GameObject openedDoor;
+    public GameObject openedDoor;
 
     // Start is called before the first frame update
     void Start()
